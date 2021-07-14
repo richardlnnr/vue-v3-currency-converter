9676bca679442c1c4f194c4b8d8f28a625d9f4fb
Richard Lindner <richard.lnnr@gmail.com>
2021-07-13 21:40:10 -0300

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-currency-converter/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue-v3-currency-converter/" : "/",
 };
